fa28ef5b2d771806e7d9b6b3ea3a8f23c5ea18cf
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2021-10-18 21:35:19 -0400

    initial commit

---
+++ install.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+color 4
+python -V
+pip -V
+echo ==================================== PYTHON VERSON ==================================
+echo.
+timeout 2
+echo.
+echo.
+color 1
+pip install pygame
+color 2
+echo.
+echo.
+@echo ======= =============== =============== =============== COMPLETED =============== =============== =============== ======
+echo.
+timeout 5
