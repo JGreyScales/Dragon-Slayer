bd1af5f5ed9e5a656f41b276a1c737643db8d6d0
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2021-10-21 20:35:30 -0400

    update

--- main.py
+++ main.py
@@ -13,18 +13,26 @@ screen = pygame.display.set_mode(size)
 level_total = 1
 
 clock = pygame.time.Clock()
-for i in range(-1, level_total - 1):
+for level in range(-1, level_total - 1):
     render_list = []
 
-    if i is -1:
-        print(level_load.render_load(i, dir))
-    while 1:
+    if level is -1:
+        render_list = level_load.render_load(i, dir)
+        print(548,348  , 722, 400)
+
+    load_level = True
+    while load_level:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-        if i is -1:
-            None
+
+            if level is -1:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.pos[0] >= 548 and event.pos[0] <= 722 and event.pos[1] >= 348 and event.pos[1] <= 400:
+                        load_level == False
         
         screen.fill((150,150,150))
+        for object in render_list:
+            screen.blit(object[0], object[1])
 
         pygame.display.flip()
         clock.tick(60)
--- scripts/level.py
+++ scripts/level.py
@@ -9,7 +9,8 @@ class level_load:
         render_list = []
 
         if level is -1:
-            play_button = [pygame.image.load(dir + r'\\Assets\\UI\\Play button.png'), (640, 120)]
+            play_button = [pygame.transform.scale(pygame.image.load(dir + r'\\Assets\\UI\\Play button.png'), (200, 75)), (540, 341)]
+            play_button.append(play_button[0].get_rect())
             render_list.append(play_button)
             
         return render_list
